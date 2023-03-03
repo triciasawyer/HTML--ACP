4cd0af8ba333ac9247c2a300294c7398bb1bd445
tektechnologies <info@tektechnologies.com>
2023-03-03 09:37:12 -0600

    add formatting

--- js/app.js
+++ js/app.js
@@ -1,55 +1,59 @@
-'use strict';
-console.log('proof of life!');
+"use strict";
+console.log("proof of life!");
 
-// function teamName() {
-//   let baseBallTeam = prompt('Do you like Cubs or Cardinals?');
+function teamName() {
+  let baseBallTeam = prompt("Do you like Cubs or Cardinals?");
 
-//   let confirmMessage = confirm('Do you want to continue?');
-//   console.log('confirm message: ', confirmMessage);
+  let confirmMessage = confirm("Do you want to continue?");
+  console.log("confirm message: ", confirmMessage);
 
-//   console.log('baseball team', baseBallTeam);
-//   let team = baseBallTeam.toLowerCase();
+  console.log("baseball team", baseBallTeam);
+  let team = baseBallTeam.toLowerCase();
 
+  let greeting;
+  if (team === "cardinals") {
+    greeting = "Right on!";
+  } else if (team === "cubs") {
+    greeting = "What a tradegy";
+  }
+  return greeting;
+}
 
-//   let greeting;
-//   if (team === 'cardinals') {
-//     greeting = 'Right on!';
-//   } else if (team === 'cubs') {
-//     greeting = 'What a tradegy';
-//   }
-//   return greeting;
-// }
+//this is getting our element from the index.html
+let imageElement = document.getElementById("imageSection");
 
+let userInput = prompt("How many seasons are there in Iowa?");
+// console.log('Our user input is of type: ',typeof(userInput));
 
-
-let imageElement = document.getElementById('imageSection');
-
-let userInput = prompt('How many seasons are there in Iowa?');
-console.log('Our user input is of type: ',typeof(userInput));
-
+//parseInt will make a string a NUMBER
 let userInputParsed = parseInt(userInput);
-console.log('user input parsed to int: ', userInputParsed)
+// console.log('user input parsed to int: ', userInputParsed)
 
 if (!isNaN(userInputParsed)) {
-  console.log('that is a valid number');
+  console.log("that is a valid number");
 } else {
-  userInputParsed = parseInt(prompt('How many seasons are there in Iowa?'));
+  userInputParsed = parseInt(prompt("How many seasons are there in Iowa?"));
 }
-console.table(typeof(userInput));
 
+console.log(typeof userInput);
+
+//declare i      conditional       i = i + 1
 for (var i = 0; i < userInputParsed; i++) {
-  let listImage = document.createElement('li');
-  let image = document.createElement('img');
-  image.setAttribute('src', 'images/states.png');
-  image.setAttribute('alt', 'pic of Iowa');
+  //this going to create and li  <li></li>
+  let listImage = document.createElement("li");
+  //creating another image <img >
+  let image = document.createElement("img");
+
+  //<img src="images/states.png"  alt=" " >
+  image.setAttribute("src", "images/states.png");
+  image.setAttribute("alt", "pic of Iowa");
   //give the image to the list
+
+  //full image
+
+  //<li><img></li>
   listImage.appendChild(image);
-  console.log(listImage);
+
+  console.log("here is the tag and image", listImage);
   imageElement.appendChild(listImage);
 }
-
-
-
-
-
-
